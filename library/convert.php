6b0bf60c2cbd5916518dd46c195158a973d81fc7
Gusnetto <gutorocher@gmail.com>
2012-10-11 11:23:47 -0300

    add update

--- library/convert.php
+++ library/convert.php
@@ -1,55 +1,39 @@
 <?php
 
-$image = new image();
-$cache = new cache();
+class Convert{
 
-$DIR_IMAGE = '/image';
-$filename = '8.png';
-$extension = 'png';
+	public function resize($filename, $width, $height) {
+		if (!file_exists(DIR_IMAGE . $filename) || !is_file(DIR_IMAGE . $filename)) {
+			return;
+		} 
 
+		$info = pathinfo($filename);
 
+		$extension = $info['extension'];
 
-public function resize($filename, $width, $height) {
+		$old_image = $filename;
+		$new_image = 'cache/' . substr($filename, 0, strrpos($filename, '.')) . '-' . $width . 'x' . $height . '.' . $extension;
 
-	if (!file_exists(DIR_IMAGE . $filename) || !is_file(DIR_IMAGE . $filename)) 
-	{
-		return;
+		if (!file_exists(DIR_IMAGE . $new_image) || (filemtime(DIR_IMAGE . $old_image) > filemtime(DIR_IMAGE . $new_image))) {
+			$path = '';
+
+			$directories = explode('/', dirname(str_replace('../', '', $new_image)));
+
+			foreach ($directories as $directory) {
+				$path = $path . '/' . $directory;
+
+				if (!file_exists(DIR_IMAGE . $path)) {
+					@mkdir(DIR_IMAGE . $path, 0777);
+				}		
+			}
+
+			$image = new Image(DIR_IMAGE . $old_image);
+			$image->resize($width, $height);
+			$image->save(DIR_IMAGE . $new_image);
+		}
+
+		return DIR_IMAGE . $new_image;
 	}
-
-	$info = pathinfo($filename);
-
-	$extension = $info['extension'];
-
-	$old_image = $filename;
-	$new_image = 'cache/' . utf8_substr($filename, 0, utf8_strrpos($filename, '.')) . '-' . $width . 'x' . $height . '.' . $extension;
-
-	if (!file_exists(DIR_IMAGE . $new_image) || (filemtime(DIR_IMAGE . $old_image) > filemtime(DIR_IMAGE . $new_image))) {
-	$path = ' ';
-
-	$directories = explode('/', dirname(str_replace('../', '', $new_image)));
-
-	foreach ($directories as $directory) {
-	$path = $path . '/' . $directory;
-
-		if (!file_exists(DIR_IMAGE . $path)) {
-			@mkdir(DIR_IMAGE . $path, 0777);
-		}	
-	}
-
-	$image = new Image(DIR_IMAGE . $old_image);
-	$image->resize($width, $height);
-	$image->save(DIR_IMAGE . $new_image);
 }
-
-	if (isset($this->request->server['HTTPS']) && (($this->request->server['HTTPS'] == 'on') || ($this->request->server['HTTPS'] == '1'))) {
-		return HTTPS_CATALOG . 'image/' . $new_image;
-	} else {
-		return HTTP_CATALOG . 'image/' . $new_image;
-	}	
-}
-
-
-
-
 
 ?>
